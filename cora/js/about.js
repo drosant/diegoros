d10f4d057bbfe049ad6b2662fe7884fc6ea2ffee
drosant <drosant.personal@gmail.com>
2024-12-11 17:40:21 +0100

    update

--- cora/js/about.js
+++ cora/js/about.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const images = ["viaje-baile.jpg", "viaje-safari.jpg"];
     
     // Tiempo de permanencia en milisegundos
-    const displayTime = 3000; // 5 segundos
+    const displayTime = 4000; // 5 segundos
 
     // Selecciona el contenedor de la imagen
     const imageContainer = document.querySelector(".down .image-container");
