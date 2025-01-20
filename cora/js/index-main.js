31729ca4a26b701664226ced55e64ee6420e9e9b
Diego Rodriguez Santibañez <diego.santibanez@aeroespacial.sener>
2025-01-20 19:00:13 +0100

    update

--- cora/js/index-main.js
+++ cora/js/index-main.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const imageContainer = document.querySelector(".image_container");
 
     const imageList = [
-        "viaje-safari4.webp", "viaje-tigre1.webp", "viaje-senora.webp", "viaje-elefante8.webp",
+        "viaje-safari4.webp", "viaje-tigre1.webp", "viaje-camarera.webp", "viaje-elefante8.webp",
         "viaje-salon.webp", "viaje-sanubari.webp", "viaje-selva.webp", "viaje-silla.webp", 
         "viaje-sillas2.webp", "viaje-sombrilla.webp", "viaje-sombrilla2.webp", "viaje-sombrilla3.webp", "viaje-surf.webp", 
         "viaje-surf1.webp", "viaje-susurro.webp", "viaje-terraza.webp", "viaje-ventana.webp", "viaje-ventana2.webp", 
